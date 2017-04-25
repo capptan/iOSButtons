83449433154e6e566173176c04b78a949f22d849
Joel Sene <joel.sene@capptan.com.br>
2017-04-25 18:16:29 -0300

    IBInspectables added, mostlly finish

--- iOSButtons.podspec
+++ iOSButtons.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'iOSButtons'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of iOSButtons.'
+  s.summary          = 'Desing flat, round and borded buttons'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,14 +18,14 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+Facilitate the use of differente types of designed buttons for iOS, eliminatiung the need of duplicating code...
                        DESC
 
-  s.homepage         = 'https://github.com/Joel Sene/iOSButtons'
+  s.homepage         = 'https://github.com/capptan/iOSButtons'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'Joel Sene' => 'joel.sene@capptan.com.br' }
-  s.source           = { :git => 'https://github.com/Joel Sene/iOSButtons.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/capptan/iOSButtons.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
@@ -37,6 +37,6 @@ TODO: Add long description of the pod here.
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+  # s.frameworks = 'UIKit'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
--- iOSButtons/Classes/Button.swift
+++ iOSButtons/Classes/Button.swift
@@ -6,4 +6,132 @@
 //
 //
 
-import Foundation
+import UIKit
+
+open class RaisedButton: Button {
+    /**
+     Prepares the view instance when intialized. When subclassing,
+     it is recommended to override the prepare method
+     to initialize property values and other setup operations.
+     The super.prepare method should always be called immediately
+     when subclassing.
+     */
+    open override func prepare() {
+        super.prepare()
+        backgroundColor = .white
+ 
+    }
+}
+
+open class Button: UIButton {
+    /**
+     A CAShapeLayer used to manage elements that would be affected by
+     the clipToBounds property of the backing layer. For example, this
+     allows the dropshadow effect on the backing layer, while clipping
+     the image to a desired shape within the visualLayer.
+     */
+    open let visualLayer = CAShapeLayer()
+    
+    @IBInspectable
+    open  var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set(value) {
+            layer.cornerRadius = value
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        prepare()
+    }
+    
+    /**
+     An initializer that initializes the object with a CGRect object.
+     If AutoLayout is used, it is better to initilize the instance
+     using the init() initializer.
+     - Parameter frame: A CGRect instance.
+     */
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        tintColor = UIColor.blue
+        prepare()
+    }
+    
+    /// A convenience initializer.
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    /**
+     A convenience initializer that acceps an image and tint
+     - Parameter image: A UIImage.
+     - Parameter tintColor: A UI
+     */
+    public convenience init(image: UIImage?, tintColor: UIColor = UIColor.blue) {
+        self.init()
+        prepare()
+    }
+    
+    /**
+     A convenience initializer that acceps a title and title
+     - Parameter title: A String.
+     - Parameter titleColor: A UI
+     */
+    public convenience init(title: String?, titleColor: UIColor = UIColor.blue) {
+        self.init()
+        prepare()
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutVisualLayer()
+        
+    }
+    
+    open func bringImageViewToFront() {
+        guard let v = imageView else {
+            return
+        }
+        bringSubview(toFront: v)
+    }
+    
+    /**
+     Prepares the view instance when intialized. When subclassing,
+     it is recommended to override the prepare method
+     to initialize property values and other setup operations.
+     The super.prepare method should always be called immediately
+     when subclassing.
+     */
+    open func prepare() {
+        prepareVisualLayer()
+    }
+}
+
+extension Button {
+    /// Prepares the visualLayer property.
+    fileprivate func prepareVisualLayer() {
+        visualLayer.zPosition = 0
+        visualLayer.masksToBounds = true
+        layer.addSublayer(visualLayer)
+    }
+}
+
+extension Button {
+    /// Manages the layout for the visualLayer property.
+    fileprivate func layoutVisualLayer() {
+        visualLayer.frame = bounds
+        visualLayer.cornerRadius = cornerRadius
+    }
+}
